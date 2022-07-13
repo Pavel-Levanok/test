5eabb631b78f2c16d6c61a4fd5b86db2b0a4a5a3
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-07-13 20:53:12 +0300

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace CodeSamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string name = "";
+string surname = "";
+int age = 0;
+int response = 0;
+
+while(response != -1)
+{
+                Console.WriteLine(@"Enter 0 to enter name 
+                                    Enter 1 to enter surname
+                                    Enter 2 to enter age
+                                    Enter -1 to exit
+                                    Enter -2 to print resume");
+
+                switch(response)
+                {
+                    case 0:
+                        { 
+                        }
+
+                    case 1:
+                        {
+
+                        }
+
+                        case -1:
+                        {
+
+                        }           
+                case
+
+
+                response = int.Parse(Console.ReadLine());
+    if(response == 0)
+    {
+        Console.WriteLine("Enter your NAME");
+        name = Console.ReadLine();
+    }
+                if (response == 1)
+                {
+                    Console.WriteLine("Enter your surname");
+                    surname = Console.ReadLine();
+                }
+
+                if (response == 2)
+                {
+                    Console.WriteLine("Enter your age");
+                    age = int.Parse(Console.ReadLine());
+                }
+
+                if (response == -2)
+                {
+                    Console.WriteLine(name);
+                    Console.WriteLine(surname);
+                    Console.WriteLine(age);
+                }
+
+
+            }
+
+Console.WriteLine(name);
+Console.ReadKey();
+
+
+        }
+    }
+}
+
+
